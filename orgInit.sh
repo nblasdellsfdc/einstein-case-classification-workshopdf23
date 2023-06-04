45ede90141d3f93b54cf1375c90c91477a39c6ea
zhou059 <zhou059@gmail.com>
2023-06-04 09:42:39 -0700

    bring to latest

--- orgInit.sh
+++ orgInit.sh
@@ -1,13 +1,13 @@
-sfdx force:org:create -f config/project-scratch-def.json -d 30 -s -w 3
+sfdx shane:org:create -f config/project-scratch-def.json -d 30 -s --wait 60 --userprefix case -o classification.einstein
 sfdx force:source:push
-sfdx force:user:password:generate
+sfdx shane:user:password:set -p salesforce1 -g User -l User
 
 #assign  AUDIT FIELD AND Einstein Case Classification User permsets
-sfdx force:user:permset:assign --permsetname Audit_Fields
-sfdx force:user:permset:assign --permsetname EinsteinAgent
+sfdx force:user:permset:assign --perm-set-name Audit_Fields
+sfdx force:user:permset:assign --perm-set-name EinsteinAgent
 
 #install Einstein Case Classification Value Analytics
-sfdx force:package:install -p 04tB0000000UQjfIAG --noprompt
+sfdx force:package:install -p 04tB0000000UQjfIAG --noprompt -w 5
 
 #bulk load Closed Cases
 sfdx shane:data:dates:update -r 09-24-2020
